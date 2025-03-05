c822f798206be4392e0d8e252352d80408a6e27f
catrinahadi <mohadi1@up.edu.ph>
2025-03-05 12:05:56 +0800

    updated test.js

--- test.js
+++ test.js
@@ -10,24 +10,24 @@ needle.post('http://localhost:3000/add-book', {
     console.log(res.body);
 }
 );
-needle.post('http://localhost:3000/add-book', {
-  bookName: 'Harry Potter and the Chamber of Secrets',
-  isbn: '0-7475-3849-2',
-  author: 'J.K Rowling',
-  yearPublished: '1998'
-}, (err, res) => {
-    console.log(res.body);
-}
-);
-needle.post('http://localhost:3000/add-book', {
-  bookName: 'The Little Prince',
-  isbn: '978-0156012195',
-  author: 'Saint-Exupery',
-  yearPublished: '1943'
-}, (err, res) => {
-    console.log(res.body);
-}
-);
+// needle.post('http://localhost:3000/add-book', {
+//   bookName: 'Harry Potter and the Chamber of Secrets',
+//   isbn: '0-7475-3849-2',
+//   author: 'J.K Rowling',
+//   yearPublished: '1998'
+// }, (err, res) => {
+//     console.log(res.body);
+// }
+// );
+// needle.post('http://localhost:3000/add-book', {
+//   bookName: 'The Little Prince',
+//   isbn: '978-0156012195',
+//   author: 'Saint-Exupery',
+//   yearPublished: '1943'
+// }, (err, res) => {
+//     console.log(res.body);
+// }
+// );
 
 // GET request by ISBN and author
 needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&amp;author=J.K+Rowling', 
